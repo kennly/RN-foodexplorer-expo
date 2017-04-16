ad1107e72c84fc6135f353aa501e2e3dd3114828
Kenneth A <kennethau82@gmail.com>
2017-04-16 23:44:52 +1000

    General: Setup Boiler Screen

---
+++ screens/AuthScreen.js
@@ -0,0 +1,17 @@
+import React, { component } from 'react',
+import { View, Text } from 'react-native';
+
+class AuthScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Auth Screen</Text>
+        <Text> Auth Screen</Text>
+        <Text> Auth Screen</Text>
+        <Text> Auth Screen</Text>
+      </View>
+    )
+  }
+}
+
+export default AuthScreen;
---
+++ screens/MapScreen.js
@@ -0,0 +1,17 @@
+import React, { component } from 'react',
+import { View, Text } from 'react-native';
+
+class MapScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> MapScreen</Text>
+        <Text> MapScreen</Text>
+        <Text> MapScreen</Text>
+        <Text> MapScreen</Text>
+      </View>
+    )
+  }
+}
+
+export default MapScreen;
