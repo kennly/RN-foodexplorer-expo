27074675a8c8eb3bfabf3af09e6ba4d56bdeb224
Kenneth A <kennethau82@gmail.com>
2017-04-17 16:26:20 +1000

    Auth: Saved user data in firebase.

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,10 +1,21 @@
 import { AsyncStorage } from 'react-native';
 import { Facebook } from 'expo';
+import firebase from 'firebase';
 
 import {
   FACEBOOK_LOGIN_SUCCESS,
   FACEBOOK_LOGIN_FAIL
 } from './types';
+
+const config = {
+    apiKey: "AIzaSyBMxI41c8SRsRcHbvizel7ZfC9mWht7OUA",
+    authDomain: "rnfirebase-da851.firebaseapp.com",
+    databaseURL: "https://rnfirebase-da851.firebaseio.com",
+    projectId: "rnfirebase-da851",
+    storageBucket: "rnfirebase-da851.appspot.com",
+    messagingSenderId: "527699718313"
+};
+firebase.initializeApp(config);
 
   //AsyncStorage store small pieces of data in phone.
   //AsyncStorage.setItem('fb_token', token);
@@ -14,31 +25,51 @@ import {
 export const facebookLogin = () =>
   async (dispatch) => {
     let token = await AsyncStorage.getItem('fb_token');
-    console.log(token, 'inside facebookLogin')
     if (token) {
       //dispatch an action to FB Login is done
       dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
     } else {
-      //start fb login process.
+      // start fb login process.
       doFacebookLogin(dispatch);
+      firebaseSave(token)
     }
   }
 
 
-
 const doFacebookLogin = async (dispatch) => {
   let { type, token } = await Facebook.logInWithReadPermissionsAsync('1885915525026805', {
-    permissions: ['public_profile']
+    permissions: ['public_profile','email']
   });
 
   if( type === 'cancel'){
+    this.props.navigation.navigate('welcome')
     return dispatch({ type: FACEBOOK_LOGIN_FAIL });
   }
 
-  await AsyncStorage.setItem('fb_token', token);
-  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+  firebaseSave(token)
+
+  await dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+
 }
 
+
+const firebaseSave = async (token) => {
+  try {
+    const credential = firebase.auth.FacebookAuthProvider.credential(token);
+    const user = await firebase.auth().signInWithCredential(credential);
+    firebase.database().ref(`/users/${user.uid}/profile`).set({
+      name: user.displayName,
+      email: user.email,
+      avatar: user.photoURL
+    })
+
+    alert('saved in firebase');
+
+    } catch (error) {
+        console.log(error.message);
+        //do something here
+    }
+}
 //code above can be refactor like this in ES6
 
 // export const facebookLogin = () =>  async dispatch => {
