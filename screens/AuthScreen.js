1d86aef62ef9def3fff8a4cfd370fa4c97a17ad5
Kenneth A <kennethau82@gmail.com>
2017-04-17 13:21:56 +1000

    Auth: Wrote reducer to pass authentication to Map screen.

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 import { connect } from 'react-redux'
 import * as actions from '../actions'
 
 class AuthScreen extends Component {
   componentDidMount() {
     this.props.facebookLogin();
+    // this temporary code will remove the storage token
+    AsyncStorage.removeItem('fb_token')
+    this.onAuthComplete(this.props);
+  }
+
+  componentWillReceiveProps(nextProps){
+    //user successfully complete authentication.
+    this.onAuthComplete(nextProps);
+  }
+
+  onAuthComplete(props) {
+    if(props.token){
+      this.props.navigation.navigate('map')
+    }
   }
 
   render() {
     return (
-      <View>
-        <Text> Auth Screen</Text>
-        <Text> Auth Screen</Text>
-        <Text> Auth Screen</Text>
-        <Text> Auth Screen</Text>
-      </View>
+      <View />
     )
   }
 }
 
-export default connect(null, actions)(AuthScreen);
+function mapStateToProps({ auth }) {
+  return { token: auth.token }
+}
+
+export default connect(mapStateToProps, actions)(AuthScreen);
