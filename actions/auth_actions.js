9cdbb765282763feac338b28453ead4df7b130c1
Kenneth A <kennethau82@gmail.com>
2017-04-17 11:57:04 +1000

    Auth: setup Facebook login logic.

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,7 +1,9 @@
 import { AsyncStorage } from 'react-native';
+import { Facebook } from 'expo';
 
 import {
-  FACEBOOK_LOGIN_SUCCESS
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL
 } from './types';
 
   //AsyncStorage store small pieces of data in phone.
@@ -13,11 +15,26 @@ export const facebookLogin = () => {
     let token = await AsyncStorage.getItem('fb_token');
     if (token) {
       //dispatch an action to FB Login is done
+      dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
     } else {
       //start fb login process.
+      doFacebookLogin(dispatch);
     }
   }
 };
+
+const doFacebookLogin = async (dispatch) => {
+  let { type, token } = await Facebook.logInWithReadPermissionAysnc('1885915525026805', {
+    permissions: ['public_profile']
+  });
+
+  if( type === 'cancel'){
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+  }
+
+  await AsyncStorage.setItem('fb_token', token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+}
 
 //code above can be refactor like this in ES6
 
