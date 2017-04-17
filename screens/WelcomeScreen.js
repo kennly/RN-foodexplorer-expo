7f191164a5b642f7be0ff05597725f1cfb03bafc
Kenneth A <kennethau82@gmail.com>
2017-04-17 10:35:26 +1000

    Welcome: Setup welcome screen style and logic

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  {text: 'Welcome to JobApp', color: '#03A9F4'},
+  {text: 'This is screen two', color: '#A6E7B3'},
+  {text: 'Set your location, then swipe away', color: '#DFE9E2'}
+];
+
 
 class WelcomeScreen extends Component {
+  constructor(props){
+    super(props)
+    this.onSlidesComplete = this.onSlidesComplete.bind(this);
+  }
+
+  onSlidesComplete(){
+    // this.props.navigation.navigate
+  }
+
   render() {
     return (
-      <View>
-        <Text> WelcomeScreen</Text>
-        <Text> WelcomeScreen</Text>
-        <Text> WelcomeScreen</Text>
-        <Text> WelcomeScreen</Text>
-      </View>
+        <Slides
+          data={SLIDE_DATA}
+          onComplete={this.onSlidesComplete}
+        />
     )
   }
 }
